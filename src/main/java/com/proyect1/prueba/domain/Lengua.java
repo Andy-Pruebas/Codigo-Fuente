9ed35e5c0e739e61d5a198dba6dbfea15686e737
Andy <andy.vilca@tecsup.edu.pe>
2019-12-06 10:54:35 -0500

    34 Commit

--- src/main/java/com/proyect1/prueba/domain/Lengua.java
+++ src/main/java/com/proyect1/prueba/domain/Lengua.java
@@ -3,7 +3,6 @@ package com.proyect1.prueba.domain;
 import javax.persistence.Column;
 import java.io.Serializable;
 
-
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
@@ -23,30 +22,29 @@ public class Lengua implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id_len")
-	private int id_len;
+	@Column(name = "id_leng")
+	private int id_leng;
 	@Column(name = "lengua")
 	private String lengua;
 
-	
 	public Lengua() {
 		super();
 	}
 
-	public Lengua(int id_len, String lengua) {
+	public Lengua(int id_leng, String lengua) {
 		super();
-		this.id_len = id_len;
+		this.id_leng = id_leng;
 		this.lengua = lengua;
 	}
 
-	public int getId_len() {
-		return id_len;
+	public int getId_leng() {
+		return id_leng;
 	}
 
-	public void setId_len(int id_len) {
-		this.id_len = id_len;
+	public void setId_leng(int id_leng) {
+		this.id_leng = id_leng;
 	}
-	
+
 	public String getLengua() {
 		return lengua;
 	}
@@ -57,7 +55,7 @@ public class Lengua implements Serializable {
 
 	@Override
 	public String toString() {
-		return "Lengua [id_len=" + id_len + ", lengua=" + lengua + "]";
+		return "Lengua [id_leng=" + id_leng + ", lengua=" + lengua + "]";
 	}
 
 }
