c5636a9a6b24f4c379519724a7fa8115848636cf
Andy <andy.vilca@tecsup.edu.pe>
2019-12-10 02:31:09 -0500

    59 Commit

--- src/main/java/com/proyect1/prueba/apis/AuthenticationController.java
+++ src/main/java/com/proyect1/prueba/apis/AuthenticationController.java
@@ -1,5 +1,7 @@
 package com.proyect1.prueba.apis;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +14,16 @@ import com.proyect1.prueba.service.AuthenticationService;
 @RestController
 @RequestMapping("/sistema")
 public class AuthenticationController {
+	private static final Logger logger=LoggerFactory.getLogger(AuthenticationController.class);
 	@Autowired
 	private AuthenticationService authenticationservice;
 	
-	@PostMapping("Login")
+	@PostMapping("/Login")
 	public User login(@RequestParam(required=false,name="usuario") String usuario, @RequestParam(required=false,name="contraseña") String contraseña){
+		logger.info("login"+usuario+" "+contraseña);
 		User user= authenticationservice.login(usuario, contraseña);
+		logger.info("Login Succes"+user);
 		return user;
+		
 	}
 }
